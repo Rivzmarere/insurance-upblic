b8195e87b8327a9736c3985403fea0d069a12db8
rivaldo1marere2@gmail.com <rivaldo1marere2@gmail.com>
2022-05-09 21:58:18 +0200

    wip

--- libs/dashboard/src/lib/service/dashboard.service.ts
+++ libs/dashboard/src/lib/service/dashboard.service.ts
@@ -9,6 +9,6 @@ export class DashboardService {
   constructor(private http: HttpClient) { }
 
   getAllPayments() {
-    return this.http.get(`http://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/`);
+    return this.http.get(`https://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/`);
   }
 }

e445e43e201582d8fb2f818187b5c7b2e44bf36e
rivaldo1marere2@gmail.com <rivaldo1marere2@gmail.com>
2022-05-09 21:29:22 +0200

    wip

--- apps/insurance/src/environments/environment.prod.ts
+++ apps/insurance/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 export const environment = {
   production: true,
+  version: require('../../../../package.json').version + '-dev',
+  auth: {
+    clientId: 'client_id',
+    clientSecret: 'client_secret'
+  },
+  baseUrl:''
 };

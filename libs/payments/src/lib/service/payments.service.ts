e0e99fa9f8b837ce454eb1d06ba8ba5b60bb7a03
rivaldo1marere2@gmail.com <rivaldo1marere2@gmail.com>
2022-05-09 23:33:33 +0200

    wip

--- libs/payments/src/lib/service/payments.service.ts
+++ libs/payments/src/lib/service/payments.service.ts
@@ -11,34 +11,34 @@ export class PaymentsService {
 
   createPayment(BeneficiaryDetails: any) {
     return this.http.post(
-      `https://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/payment/post-payment`,
+      `http://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/payment/post-payment`,
       BeneficiaryDetails
     );
   }
   getAllPayments() {
-    return this.http.get(`https://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/payment/view-all-payements`);
+    return this.http.get(`http://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/payment/view-all-payements`);
   }
 
   getCarByRegNumber(regNumber: any) {
     return this.http.get(
-      `https://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/car/get-car-by-regNumber/${regNumber}`
+      `http://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/car/get-car-by-regNumber/${regNumber}`
     );
   }
   getPaymentsByCarID(carId: any) {
     return this.http.get(
-      `https://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/payment/get-payment-by-vehcleId/${carId}`
+      `http://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/payment/get-payment-by-vehcleId/${carId}`
     );
   }
 
   getInsuranceByName(name: any) {
     return this.http.get(
-      `https://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/insurance/get-insurance-by-name/${name}`
+      `http://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/insurance/get-insurance-by-name/${name}`
     );
   }
 
   getCarById(id: any) {
     return this.http.get(
-      `https://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/car/get-car-by-id/${id}`
+      `http://hit200node-env.eba-arzr3fni.us-east-1.elasticbeanstalk.com/car/get-car-by-id/${id}`
     );
   }
 }
